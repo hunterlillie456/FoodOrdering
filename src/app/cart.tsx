dbcf82101966dfd8cfd90fa84632862447316b8c
Lillie Hunter <lillie@Lillies-iMac.local>
2024-02-20 19:39:54 -0500

    Saving before everything goes to crap

---
+++ src/app/cart.tsx
@@ -0,0 +1,15 @@
+import { View, Text, Platform } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+
+const CartScreen = () => {
+    return (
+        <View>
+            <Text>cart</Text>
+
+            <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
+        </View>
+    )
+}
+
+export default CartScreen;
